7a2171853d3ebb1bce1b82e3ca2875a0bfdbc0fe
RicardoATB <ricardoatb@gmail.com>
2020-01-14 22:32:37 -0500

    checked board!

--- javascript/maze.js
+++ javascript/maze.js
@@ -1,17 +1,45 @@
 //Global variables
 var cols, rows;
 var cellWidth = 40;
-
+var cellArray = [];
+/*****************************************************************************/
 function setup() {
 	createCanvas(400,400);
-}
 
+	//Calculating how many cols and rows, and making it an integer with floor()
+	cols = floor(width/cellWidth);
+	rows = floor(height/cellWidth);
+
+	//Creating all the cell objects with a nested loop
+	for(var i = 0; i < rows; i++)
+		for(var j = 0; j < cols; j++ ) {
+			var cell = new Cell(i,j);
+
+			//Storing all the cell objects in an unidimensional array
+			cellArray.push(cell);
+		}
+}
+/*****************************************************************************/
 function draw() {
-	background(50);
-}
+	background(100);
 
-//Constructor for the cell object
-function cell(x,y){
-	this.x = x;
-	this.y = y;
+	//displaying all the cell objects:
+	for (var i = 0; i < cellArray.length; i++)
+		cellArray[i].showCell();
 }
+/*****************************************************************************/
+//Constructor for the cell object
+function Cell(i,j){
+	this.i = i;
+	this.j = j;
+
+	this.showCell = function() {
+		var x = this.i*cellWidth;
+		var y = this.j*cellWidth;
+		stroke(255);
+		noFill();
+		rect(x,y,cellWidth,cellWidth);
+	}
+
+}
+/*****************************************************************************/
