ae21dc147b6ddbfed699679b5b79b9ce656b1675
ricardoatb@gmail.com <ricardoatb@gmail.com>
2020-05-19 09:28:09 -0400

    working project

--- scripts/maze.js
+++ scripts/maze.js
@@ -3,6 +3,9 @@ var cols, rows; //number of columns and rows
 var cellWidth = 40; // height = width of each square cell
 var cellArray = []; // unidimensional array that will store all the cells
 var currentCell; // this is the cell that is current being visited
+var stack = []; // creating a stack (empty array)
+
+p5.disableFriendlyErrors = true; //disables FES to improve performance
 /*****************************************************************************/
 function setup() {
 	createCanvas(400,400);
@@ -12,7 +15,7 @@ function setup() {
 	rows = floor(height/cellWidth); // number of rows
 
 	//setting framerate
-	frameRate(5);
+	frameRate(7);
 
 	//Creating all the cell objects with a nested loop
 	for(var j = 0; j < rows; j++)
@@ -38,6 +41,7 @@ function draw() {
 
 	//if we are drawing the cell, it means that it has been visited	
 	currentCell.visited = true;
+	currentCell.highlight();
 
 	var nextCell = currentCell.checkNeighbors(); // nextCell is one of the
 												 // available neighbors
@@ -45,9 +49,36 @@ function draw() {
 	//visiting all the neighbors
 	if (nextCell) {
 		nextCell.visited = true; //mark nextCell as visited
+		stack.push(currentCell);
+		removeWalls(currentCell, nextCell);
 		currentCell = nextCell;	 //nextCell is now the current cell
+	} else if (stack.length > 0) { // stack is not empty
+		currentCell = stack.pop();
 	}
 
+}
+/*****************************************************************************/
+function removeWalls (a, b) {
+
+	var x = a.i - b.i;
+
+	if ( x === 1) {
+		a.walls[3] = false; // left wall removed
+		b.walls[1] = false; // right wall removed
+	} else if ( x === -1 ) {
+		a.walls[1] = false; // right wall removed
+		b.walls[3] = false; // left wall removed
+	}
+
+	var y = a.j - b.j;
+
+	if ( y === 1) {
+		a.walls[0] = false; // top wall removed
+		b.walls[2] = false; // bottom wall removed
+	} else if ( y === -1 ) {
+		a.walls[2] = false; // bottom wall removed
+		b.walls[0] = false; // top wall removed
+	}
 
 }
 /*****************************************************************************/
@@ -115,7 +146,7 @@ function Cell(i,j){
 
 
 
-
+		
 		//selecting randomly a neighbor to be the next cell to be visited
 		if (neighbors.length > 0) {
 			var r = floor(random(0, neighbors.length));
@@ -123,8 +154,28 @@ function Cell(i,j){
 		}
 		else	
 			return undefined; // this line should never run
+		
+
+
+		/*
+		//selecting first neighbor to be the next cell to be visited
+		if (neighbors.length > 0)
+			return neighbors[0];
+		*/
+	
+	}
+	/*-----------------------------------------------------------------------*/
+	// Draws another rectangle to show which is the current cell
+	this.highlight = function() {
+
+		var x = this.i*cellWidth;
+		var y = this.j*cellWidth;
+		noStroke();
+		fill(0, 0, 255, 100);
+		rect(x, y, cellWidth, cellWidth);
 
 	}
+
 	/*-----------------------------------------------------------------------*/
 	this.showCell = function() {
 		var x = this.i*cellWidth;
@@ -163,8 +214,9 @@ function Cell(i,j){
 	
 		// if the cell has been visited, change it's color
 		if (this.visited) {
-		fill(255,0,255,100);
-		rect(x,y,cellWidth,cellWidth);
+			noStroke();
+			fill(255,0,255,100);
+			rect(x,y,cellWidth,cellWidth);
 		}
 		/*-----------------------------------------------------------------------*/
 	}
